190c7df3d4b3504b9fae3b568b120628c8be2ffa
yuwol <yuwol036@gmail.com>
2024-07-14 20:23:49 +0900

    🌱 Seed courses

--- packages/api/src/db/seed.ts
+++ packages/api/src/db/seed.ts
@@ -2,14 +2,38 @@ import { categories } from '@/data/category'
 import { departments } from '@/data/department'
 import { queryClient } from '@/db'
 import { createCategories, removeAllCategories } from '@/db/query/category'
+import { createCourses, removeAllCourses } from '@/db/query/course'
 import { createDepartments, removeAllDepartments } from '@/db/query/department'
+import { FetchCoursesOptions, fetchParsedCourses } from '@/script/crawl'
 
 const seed = async () => {
+  await removeAllCourses()
   await removeAllDepartments()
   await removeAllCategories()
 
   await createDepartments(departments)
   await createCategories(categories)
+
+  const options = [
+    { year: 2024, semester: 'spring' },
+    { year: 2024, semester: 'summer' },
+    { year: 2024, semester: 'fall' },
+  ] as const satisfies FetchCoursesOptions[]
+
+  for (const option of options) {
+    const courses = await fetchParsedCourses(option)
+
+    if (!courses) {
+      console.log('Failed to fetch courses with options: ', option)
+      continue
+    }
+
+    // Batch queries to avoid maximum call stack size exceeded error
+    const batchSize = 1000
+    for (let i = 0; i < courses.length; i += batchSize) {
+      await createCourses(courses.slice(i, i + batchSize))
+    }
+  }
 
   await queryClient.end()
 }
